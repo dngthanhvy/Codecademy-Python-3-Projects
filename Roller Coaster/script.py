cb7fb6279dc27bd76aeffef987a5a273e73ff783
Thanh-Vy Duong <dng.thanhvy@gmail.com>
2021-02-26 19:33:45 +0100

    added 3 funcs

--- Roller Coaster/script.py
+++ Roller Coaster/script.py
@@ -2,102 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load rankings data here:
-
-
-
-
-
-# write function to plot rankings over time for 1 roller coaster here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# write function to plot rankings over time for 2 roller coasters here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# write function to plot top n rankings over time here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# load roller coaster data here:
-
-
-
-# write function to plot histogram of column values here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# write function to plot inversions by coaster at a park here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# write function to plot pie chart of operating status here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
-
-# write function to create scatter plot of any two numeric columns here:
-
-
-
-
-
-
-
-
-
-
-plt.clf()
+steel_winners = pd.read_csv('Golden_Ticket_Award_Winners_Steel.csv')
+wood_winners = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
+roller_coasters = pd.read_csv('roller_coasters.csv')
+
+
+# ==============================================================
+# FUNCTIONS
+# ==============================================================
+def plot_1ranking_over_time(roller_coaster_name, park_name, df_ranking):
+    roller_coaster = df_ranking[(df_ranking['Name'] == roller_coaster_name) & (df_ranking['Park'] == park_name)]
+    plt.plot(roller_coaster['Year of Rank'], roller_coaster.Rank)
+    plt.ylabel('Ranking')
+    plt.gca().invert_yaxis()
+    plt.xlabel('Year')
+    plt.title("{} Rankings".format(roller_coaster_name))
+    plt.show()
+
+
+def plot_2ranking_over_time(roller_coaster_name1, park_name1, roller_coaster_name2, park_name2, df_ranking):
+    plot_1ranking_over_time(roller_coaster_name1, park_name1, df_ranking)
+    plot_1ranking_over_time(roller_coaster_name2, park_name2, df_ranking)
+    plt.gca().invert_yaxis() # the invert yaxis was called twice so to revert it it has to be called once more
+    plt.title("{} and {} Rankings".format(roller_coaster_name1, roller_coaster_name2))
+    plt.legend([roller_coaster_name1, roller_coaster_name2])
+    plt.show()
+
+
+def plot_n_rank(n, df_ranking):
+    top_n_ranking = df_ranking[df_ranking['Rank'] <= n]
+    for coaster in set(top_n_ranking['Name']):
+        rankings = top_n_ranking[top_n_ranking['Name'] == coaster]
+        plt.plot(rankings['Year of Rank'], rankings['Rank'])
+
+    plt.legend(list(set(top_n_ranking['Name'])))
+    plt.gca().invert_yaxis()
+    plt.show()
+
+# ==============================================================
+# MAIN CALLS
+# ==============================================================
